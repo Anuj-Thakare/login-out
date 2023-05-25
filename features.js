83a3554e3b6c629d173c048895b0b8d3f1511a6d
Anuj Thakare <anujthakare19@gmail.com>
2023-05-25 20:06:16 +0530

    Done

--- features.js
+++ features.js
@@ -3,7 +3,7 @@
 // const name2= "Ganesh";
 // export default name;
 // export {name1,name2};
-const generate=()=>{
+const generate = () => {
     return `${Math.floor(Math.random()*100)}%`
 }
 export default generate;
